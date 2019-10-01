f91426c40a80ad2f4fec66026b6c6a2a2c4fc249
David Pérez <41592808e@institutmontilivi.cat>
2019-10-01 16:00:38 +0200

    Commit inicial

---
+++ Llista de jocs/Llista de jocs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Llista_de_jocs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
